0daf3aadad082cc4279705327405bf28c5788041
Ben <benxian456@gmail.com>
2022-11-06 13:59:26 +0800

    update for github view

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const server= express();
 
 //server.use(express.static("index.html"));
-
+server.get("/redirect",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
 server.use(express.static(__dirname + '/public'));
 // server.get("/",(req,res)=>{
 
